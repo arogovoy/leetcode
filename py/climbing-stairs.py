3746c52252165a7d817b445414927adfc76c8619
Alexander Rogovoy <rogovoyalexandr94@gmail.com>
2022-09-09 11:21:43 +0300

    climbStairs

---
+++ py/climbing-stairs.py
@@ -0,0 +1,13 @@
+# 70. Climbing Stairs
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        return n
+
+
+if __name__ == '__main__':
+    res = Solution().climbStairs(2)
+    print(res)  # 2
+
+    res = Solution().climbStairs(3)
+    print(res)  # 3
