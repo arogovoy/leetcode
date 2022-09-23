f5eea2f9b988a6d85285765d325dbea3ce82fab7
Alexander Rogovoy <rogovoyalexandr94@gmail.com>
2022-09-23 10:32:12 +0300

    climbing-stairs

--- py/climbing-stairs.py
+++ py/climbing-stairs.py
@@ -2,12 +2,15 @@
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return n
+        sum_left, sum_right = 1, 1
+        for x in range(2, n + 1, 1):
+            sum_right, sum_left = sum_left, sum_left + sum_right
+        return sum_left
 
 
 if __name__ == '__main__':
     res = Solution().climbStairs(2)
     print(res)  # 2
 
-    res = Solution().climbStairs(3)
+    res = Solution().climbStairs(6)
     print(res)  # 3
